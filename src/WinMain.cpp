9a1f9ca849c0b0e3e451d3a0412a296b675c77af
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2026-05-12 00:50:51 +0200

    not bad

--- src/WinMain.cpp
+++ src/WinMain.cpp
@@ -1,11 +1,37 @@
 #include <windows.h>
 
+
+HWND hInput = nullptr; 
+HWND mWindow = nullptr;
+
+// TODO Add ID check on case for HWND 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     switch (msg)
     {
+
+    case WM_CREATE:
+        hInput = CreateWindowEx(
+                WS_EX_CLIENTEDGE,      
+                L"EDIT",                
+                L"",                    
+                WS_CHILD | WS_VISIBLE,  
+                10, 10,                 
+                400, 30,                
+                hwnd,                   
+                (HMENU)101,
+                nullptr, nullptr
+            );
+        return 0;
+
+    case WM_CLOSE:
+        if ( mWindow && mWindow == hwnd ){
+            DestroyWindow(hwnd);
+        }
+        return 0;
+        
     case WM_DESTROY:
-        PostQuitMessage(0);
+        PostQuitMessage(0); // leave msg loop
         return 0;
     }
     return DefWindowProc(hwnd, msg, wParam, lParam);
@@ -24,7 +50,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
     wc.hCursor       = LoadCursor(nullptr, IDC_ARROW);
     RegisterClassEx(&wc);
 
-    HWND hwnd = CreateWindowEx(
+    mWindow = CreateWindowEx(
         0,
         L"TestWindow",
         L"XXXX",
@@ -32,23 +58,23 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
         100, 100, 600, 400,
         nullptr, nullptr, hInstance, nullptr
     );
-    SetWindowDisplayAffinity(hwnd, 0x00000011); // WDA_EXCLUDEFROMCAPTURE
-    ShowWindow(hwnd, nCmdShow);
-    UpdateWindow(hwnd);
+    SetWindowDisplayAffinity(mWindow, 0x00000011); // WDA_EXCLUDEFROMCAPTURE
+    ShowWindow(mWindow, nCmdShow);
+    UpdateWindow(mWindow);
 
-    HWND hwnd2 = CreateWindowEx(
-        0,
-        L"TestWindow",
-        L"XXXX",
-        WS_OVERLAPPEDWINDOW,
-        100, 100, 600, 1200,
-        nullptr, nullptr, hInstance, nullptr
-    );
-    SetWindowDisplayAffinity(hwnd2, 0x00000011);
+    // HWND hwnd2 = CreateWindowEx(
+    //     0,
+    //     L"TestWindow",
+    //     L"XXXX",
+    //     WS_OVERLAPPEDWINDOW,
+    //     100, 100, 600, 1200,
+    //     nullptr, nullptr, hInstance, nullptr
+    // );
+    // SetWindowDisplayAffinity(hwnd2, 0x00000011);
 
 
-    ShowWindow(hwnd2, nCmdShow);
-    UpdateWindow(hwnd2);
+    // ShowWindow(hwnd2, nCmdShow);
+    // UpdateWindow(hwnd2);
 
     MSG msg = {};
     while (GetMessage(&msg, nullptr, 0, 0))
