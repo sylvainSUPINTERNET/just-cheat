32b9b77719721dc1803d849a6da57c3a5a9e1a42
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2026-05-12 00:23:08 +0200

    wip

--- src/WinMain.cpp
+++ src/WinMain.cpp
@@ -1,14 +1,48 @@
 #include <windows.h>
-#include <print>
-#include <ostream>
-#include <format>
-#include <iostream>
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-    // std::cin.get();
+    switch (msg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
+{
     MessageBox(nullptr, L"hello", L"Test", MB_OK);
-   
+
+    WNDCLASSEX wc    = {};
+    wc.cbSize        = sizeof(wc);
+    wc.lpfnWndProc   = WndProc;
+    wc.hInstance     = hInstance;
+    wc.lpszClassName = L"TestWindow";
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wc.hCursor       = LoadCursor(nullptr, IDC_ARROW);
+    RegisterClassEx(&wc);
+
+    HWND hwnd = CreateWindowEx(
+        0,
+        L"TestWindow",
+        L"XXXX",
+        WS_OVERLAPPEDWINDOW,
+        100, 100, 600, 400,
+        nullptr, nullptr, hInstance, nullptr
+    );
+
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
+
+    MSG msg = {};
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
     return 0;
 }
 
